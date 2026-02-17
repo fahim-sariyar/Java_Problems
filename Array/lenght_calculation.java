a39dc5fc8359fad1db4f2ac83263e09e8e99b108
FAHIM SARIYAR SHOVON <165449904+fahim-sariyar@users.noreply.github.com>
2026-02-17 19:42:46 +0600

    Create lenght_calculation.java

---
+++ Array/lenght_calculation.java
@@ -0,0 +1,9 @@
+public class length_calculate 
+{
+    public static void main(String[] args) 
+  {
+        int marks[] = new int[1000];
+    
+        System.out.println("lenth of the array: " + marks.length);
+    }
+}
