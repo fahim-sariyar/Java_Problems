6f52368d6da887de45b9efd7219d257fe80d4bc2
FAHIM SARIYAR SHOVON <165449904+fahim-sariyar@users.noreply.github.com>
2026-03-02 23:41:11 +0600

    Create arrayList.java

---
+++ Collection Framework/List/Array List/arrayList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class arrayList 
+{
+    public static void main(String[] args) {
+        ArrayList<String> StudentName = new ArrayList<>();
+        StudentName.add("Fahim Sariyar Shovon");
+        StudentName.add("Labib");
+        StudentName.add("Shawon");
+        StudentName.add("Dolon");
+
+        System.out.println(StudentName);
+    }
+}
