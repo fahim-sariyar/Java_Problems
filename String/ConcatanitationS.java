307edbdd1ad0219f1a59121304e3c02e5f280a7d
FAHIM SARIYAR SHOVON <165449904+fahim-sariyar@users.noreply.github.com>
2026-03-05 23:40:44 +0600

    Add ConcatanitationS class for string concatenation

---
+++ String/ConcatanitationS.java
@@ -0,0 +1,9 @@
+class ConcatanitationS {
+    public static void main(String[] args) 
+    {
+        String firstName = "Fahim";
+        String lastName = "Sariyar";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
