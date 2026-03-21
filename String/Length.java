bc8e360b7a70af8a2317a0ec032a30d1ac891976
FAHIM SARIYAR SHOVON <165449904+fahim-sariyar@users.noreply.github.com>
2026-03-21 23:42:11 +0600

    Update Length.java

--- String/Length.java
+++ String/Length.java
@@ -1,6 +1,6 @@
 public class length_code {
     public static void main(String[] args) {
-        String name = "Rakibul Hasan Shuvo";
+        String name = "Fahim Sariyar Shovon ";
         int result = name.length();
         System.out.println(result);
     }
