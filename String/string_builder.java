96d4feec07c09d9b5fc24b92737a47beec2b28b8
FAHIM SARIYAR SHOVON <165449904+fahim-sariyar@users.noreply.github.com>
2026-03-27 21:05:06 +0600

    Create string_builder.java

---
+++ String/string_builder.java
@@ -0,0 +1,13 @@
+public class string_builder 
+{
+    public static void main(String[] args) 
+  {
+        StringBuilder sb = new StringBuilder("");
+        for(char ch = 'a'; ch <= 'z'; 
+            ch++) 
+        {
+            sb.append(ch);
+        }
+        System.out.print(sb);
+    }
+}
