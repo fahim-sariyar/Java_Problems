c57afbaf0b5eab78ea3fb547080bea50f0221198
FAHIM SARIYAR SHOVON <165449904+fahim-sariyar@users.noreply.github.com>
2026-03-28 20:04:01 +0600

    Add Java program for book details management

---
+++ BUBT /Task-Java-Book-Details-Program.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Book b = new Book();
+        b.setData("Java Programming", 450.50);
+        b.showData();
+    }
+}
+
+class Book {
+    String title;   double price;
+    void setData(String t, double p) {
+        title = t; price = p;
+    }
+
+    void showData() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Book Price: " + price);
+    }
+}
